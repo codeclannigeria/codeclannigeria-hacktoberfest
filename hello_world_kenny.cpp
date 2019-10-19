5dc6b8e3c6eaedf3fef2ebcd69d3fdc9a50a88b2
BobadeKenny <50062439+BobadeKenny@users.noreply.github.com>
2019-10-19 06:54:57 +0100

    Hello World in C++

---
+++ hello_world_kenny.cpp
@@ -0,0 +1,15 @@
+/*
+Language: C++
+Author:Bobade Kehinde
+Github: https://github.com/BobadeKenny
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
