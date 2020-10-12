b721d043e032d57193d08f8868cf01e9df9c3f3d
Meet Patel <pmeet7895@gmail.com>
2020-10-12 13:16:08 +0530

    Create hello_world_meetpatel.js

---
+++ scripts/hello_world_meetpatel.js
@@ -0,0 +1,7 @@
+// LANGUAGE: Javascript
+// ENV: ES6
+// AUTHOR: Meet Patel
+// GITHUB: https://github.com/meetxpress
+
+console.log('Greeting Hello World to you.');
+console.log('From Meet Patel');
