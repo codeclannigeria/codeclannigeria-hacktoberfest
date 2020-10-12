fed43f617a4fb5c1295df79b891ace1752d65406
Meet Patel <pmeet7895@gmail.com>
2020-10-12 13:20:14 +0530

    Update and rename hello_world_meetpatel.js to hello_world_meetpatel.kt

--- scripts/hello_world_meetpatel.js
+++
@@ -1,7 +0,0 @@
-// LANGUAGE: Javascript
-// ENV: ES6
-// AUTHOR: Meet Patel
-// GITHUB: https://github.com/meetxpress
-
-console.log('Greeting Hello World to you.');
-console.log('From Meet Patel');
---
+++ scripts/hello_world_meetpatel.kt
@@ -0,0 +1,9 @@
+// LANGUAGE: Kotlin
+// ENV: Android
+// AUTHOR: Meet Patel
+// GITHUB: https://github.com/meetxpress
+
+fun main(args: Array<String>) {
+  println("Greeting Hello World to you.")    
+  println("From Meet Patel");
+}
